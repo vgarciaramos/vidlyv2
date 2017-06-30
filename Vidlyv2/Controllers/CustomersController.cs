c638aa19f184ae6f69edecb0b1f91be962168d6f
Victor Garcia <v.garciaramos@hotmail.com>
2017-06-30 11:16:13 -0500

    ejercicio2

---
+++ Vidlyv2/Controllers/CustomersController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidlyv2.Models;
+namespace Vidlyv2.Controllers
+{
+    public class CustomersController : Controller
+    {
+
+        //*-*-*-*-* Ejercicio 2
+        public ViewResult Index()
+        {
+
+            var customers = GetCustomers();
+            return View(customers);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customers = GetCustomers().SingleOrDefault(c => c.Id == id);
+            if (customers == null)
+                return HttpNotFound();
+
+
+            return View(customers);
+        }
+
+        private IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer> { 
+                new Customer{Id = 1, Name = "John Smith"},
+                new Customer{Id = 2, Name = "Mary Williams"}
+            };
+        }
+
+
+        
+
+        //public ActionResult Random()
+        //{
+        //    var customers = new List<Customer>
+        //    {
+        //        new Customer{ Name = "John Smith"},
+        //        new Customer{ Name = "Mary Williams"}
+        //    };
+
+        //    return View(customers);
+        //}
+    }
+}
---
+++ Vidlyv2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Vidlyv2.Startup))]
+namespace Vidlyv2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
